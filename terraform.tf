9aa7190892d70db2720f46d0c97ceb9a68fa8203
Francesco Di Sales <f.disales@hotmail.com>
2026-02-01 12:32:21 +0100

    Add Terraform configuration for Linode instances
    This Terraform configuration defines the Linode provider and resources for three instances named miner1, miner2, and miner3, including their respective configurations and variables.

---
+++ terraform.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    linode = {
+      source = "linode/linode"
+      version = "3.8.0"
+    }
+  }
+}
+
+variable "tokenlinode" {
+
+  type = string
+
+}
+
+variable "passwords" {
+
+  type = list(string)
+
+}
+
+variable "instances" {
+
+  type = list(string)
+
+}
+
+provider "linode" {
+  # Configuration options
+  token = var.tokenlinode
+}
+
+
+
+resource "linode_instance" "miner1" {
+  label           = "miner1"
+  image           = "linode/Ubuntu25.10"
+  region          = var.region
+  type            = var.instances[0]
+
+  root_pass       = var.passwords[0]
+
+  tags       = ["miner1"]
+  swap_size  = 256
+  private_ip = true
+}
+
+
+resource "linode_instance" "miner2" {
+  label           = "miner2"
+  image           = "linode/Ubuntu25.10"
+  region          = var.region
+  type            = var.instances[1]
+
+  root_pass       = var.passwords[1]
+
+  tags       = ["miner2"]
+  swap_size  = 256
+  private_ip = true
+
+}
+
+
+
+resource "linode_instance" "miner3" {
+  label           = "miner1"
+  image           = "linode/Ubuntu25.10"
+  region          = var.region
+  type            = var.instances[2]
+
+  root_pass       = var.passwords[2]
+
+  tags       = ["miner3"]
+  swap_size  = 256
+  private_ip = true
+
+}
+
